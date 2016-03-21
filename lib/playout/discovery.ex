7784d1590d3b78970f50e0abdf28f4e6feaf1e0a
4pcbr <me@4pcbr.com>
2016-03-21 21:25:58 +0100

    Implemented discovery service

--- lib/playout/discovery.ex
+++ lib/playout/discovery.ex
@@ -2,12 +2,35 @@ defmodule Playout.Discovery do
 
   alias ExSlp.Service
 
-  def start_link do
-    case Service.registered? do
+  if Mix.env == :dev do
+    @slp_args [ u: "127.0.0.1" ]
+  else
+    @slp_args []
+  end
+
+  def discover do
+    case Service.registered?( @slp_args, [] ) do
       false ->
         { :ok, _ } = Service.register
       _ -> nil
     end
+    Service.discover( @slp_args, [] )
+      |> ensure_connnected
+    :timer.sleep( 10000 )
+    discover
+  end
+
+  def start_link do
+    pid = spawn_link( __MODULE__, :discover, [] )
+    { :ok, pid }
+  end
+
+  defp ensure_connnected( services ) do
+    service_map = services
+      |> Enum.map( fn service ->
+        { service, true }
+      end ) |> Enum.into(%{})
+    IO.inspect service_map
   end
 
 end

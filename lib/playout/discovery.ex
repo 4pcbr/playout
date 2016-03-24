d5764587c94db6814a4e120685799d1526508299
4pcbr <me@4pcbr.com>
2016-03-24 22:15:06 +0100

    Removed discovered* mess

--- lib/playout/discovery.ex
+++ lib/playout/discovery.ex
@@ -1,5 +1,7 @@
 defmodule Playout.Discovery do
 
+  use GenServer
+
   alias ExSlp.Service
 
   if Mix.env == :test do
@@ -8,7 +10,7 @@ defmodule Playout.Discovery do
     @slp_args []
   end
 
-  def discover do
+  def handle_info( :discover, state ) do
     case Service.registered?( @slp_args, [] ) do
       false ->
         { :ok, _ } = Service.register
@@ -16,13 +18,40 @@ defmodule Playout.Discovery do
     end
     Service.discover( @slp_args, [] )
       |> ensure_connnected
-    :timer.sleep( 10000 )
-    discover
+    Process.send_after( __MODULE__, :discover, 10000 )
+    { :noreply, state }
   end
 
-  def start_link do
-    pid = spawn_link( __MODULE__, :discover, [] )
-    { :ok, pid }
+  def update( services ) do
+    GenServer.cast( __MODULE__, { :update, services } )
+  end
+
+  def get_services do
+    GenServer.call( __MODULE__, :get_services )
+  end
+
+  def handle_cast({ :update, services }, _state ) do
+    # TODO: compare the sets and notify the observers
+    { :noreply, :ordsets.from_list( services ) }
+  end
+
+  def handle_call( :get_services, _from, state ) do
+    { :reply, state, state }
+  end
+
+  def handle_info({ :DOWN, _ref, :process, _pid, _reason }, state ) do
+    Service.deregister
+    { :noreply, state }
+  end
+
+  def terminate( _reason, state ) do
+    Service.deregister
+  end
+
+  def start_link( state ) do
+    res = { :ok, _ } = GenServer.start_link( __MODULE__, state, name: __MODULE__ )
+    Process.send( __MODULE__, :discover, [] )
+    res
   end
 
   defp ensure_connnected( services ) do

3670487be95c745b5ed4ecf4b2c3a006f58d3937
4pcbr <me@4pcbr.com>
2016-03-31 22:52:45 +0200

    Fixed nodes controller test a bit: testing the resp_body now

--- test/controllers/node_controller_test.exs
+++ test/controllers/node_controller_test.exs
@@ -1,11 +1,12 @@
 defmodule Playout.NodeControllerTest do
   use Playout.ConnCase
+  use ExUnit.Case, async: true
 
   @tag controller: true, api: true
   test "GET /api/nodes", %{ conn: conn } do
     response = get( conn, "/api/nodes" )
     assert response.status == 200
-    IO.inspect conn
+    assert response.resp_body == "[\"service:exslp://node@192.168.178.22,65535\"]"
   end
 
 end
